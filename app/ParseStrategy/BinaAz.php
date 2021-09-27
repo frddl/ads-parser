acec94c8f0db93ae74ce0723dfe5c75199ed5bcf
Farid Mammadov <faridmmmdv@gmail.com>
2021-09-27 17:32:27 +0400

    htmldom

--- app/ParseStrategy/BinaAz.php
+++ app/ParseStrategy/BinaAz.php
@@ -2,12 +2,16 @@
 
 namespace App\ParseStrategy;
 
+use FastSimpleHTMLDom\Document;
+
 class BinaAz extends MasterStrategy
 {
     public $conf_path = 'parsers.sites.bina_az';
 
     public function parse(): array
     {
-        return [];
+        $html = new Document(file_get_contents($this->getConfig()['url']));
+        $ret = $html->find('div[id=foo]');
+        return $ret;
     }
 }
