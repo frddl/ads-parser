a65d12873db607eb696790f57e6a1534a3e7e6e3
Farid Mammadov <faridmmmdv@gmail.com>
2021-10-01 12:19:31 +0400

    added system status check

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Jobs\ParseUpdatesJob;
 use App\Models\AdItem;
+use App\Settings\GeneralSettings;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,29 +27,33 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $periods = config('parsers.periods');
-        foreach ($periods as $period) {
-            $items = AdItem::every($period)->active()->get();
-            foreach ($items as $item) {
-                switch ($period) {
-                    case 1:
-                        $schedule->job(new ParseUpdatesJob($item))->everyMinute();
-                        break;
-                    case 5:
-                        $schedule->job(new ParseUpdatesJob($item))->everyFiveMinutes();
-                        break;
-                    case 15:
-                        $schedule->job(new ParseUpdatesJob($item))->everyFifteenMinutes();
-                        break;
-                    case 30:
-                        $schedule->job(new ParseUpdatesJob($item))->everyThirtyMinutes();
-                        break;
-                    case 60:
-                        $schedule->job(new ParseUpdatesJob($item))->hourly();
-                        break;
-                    case 120:
-                        $schedule->job(new ParseUpdatesJob($item))->everyTwoHours();
-                        break;
+        $system_is_active = app(GeneralSettings::class)->is_active;
+
+        if ($system_is_active) {
+            $periods = config('parsers.periods');
+            foreach ($periods as $period) {
+                $items = AdItem::every($period)->active()->get();
+                foreach ($items as $item) {
+                    switch ($period) {
+                        case 1:
+                            $schedule->job(new ParseUpdatesJob($item))->everyMinute();
+                            break;
+                        case 5:
+                            $schedule->job(new ParseUpdatesJob($item))->everyFiveMinutes();
+                            break;
+                        case 15:
+                            $schedule->job(new ParseUpdatesJob($item))->everyFifteenMinutes();
+                            break;
+                        case 30:
+                            $schedule->job(new ParseUpdatesJob($item))->everyThirtyMinutes();
+                            break;
+                        case 60:
+                            $schedule->job(new ParseUpdatesJob($item))->hourly();
+                            break;
+                        case 120:
+                            $schedule->job(new ParseUpdatesJob($item))->everyTwoHours();
+                            break;
+                    }
                 }
             }
         }
